cac897700c16b0d61fbe3eca356e3bd89c050b2b
Ansh-1306 <patelansh1306@gmail.com>
2024-08-27 04:58:51 +0530

    Offer Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Popular from "./components/Popular";
+import Offers from "./components/Offers";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { useEffect } from "react";
@@ -18,6 +19,7 @@ export default function App() {
       <Header />
       <Hero />
       <Popular />
+      <Offers />
     </>
   )
 }
